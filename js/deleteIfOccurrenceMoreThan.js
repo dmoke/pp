acc90eeec2f85121c1cb6cd928a85e87ad25701c
dmoke <dmytromykhaliukk@gmail.com>
2026-02-14 23:43:27 +0200

    bef order

--- js/deleteIfOccurrenceMoreThan.js
+++ js/deleteIfOccurrenceMoreThan.js
@@ -1,12 +1,20 @@
 function deleteNth(arr, n) {
   [...new Set(arr)].forEach((e) => {
-    let occurrences = arr.reduce((acc, next) => (next === e ? acc + 1 : acc));
-    if (n < occurrences) arr = arr
-        .join("")
-        .replace(new Array(occurrences).fill(e), new Array(n).fill(e))
-        .split("");
+    let occurrences = arr
+      .sort((a, b) => a - b)
+      .reduce((acc, next) => (next === e ? acc + 1 : acc), 0);
+
+    if (n < occurrences)
+      arr = arr
+        .join(",")
+        .replace(
+          new Array(occurrences).fill(e).join(","),
+          new Array(n).fill(e).join(","),
+        )
+        .split(",")
+        .map(Number);
   });
   return arr;
 }
 
-console.log(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 1));
+console.log(deleteNth([20,37,20,21], 1));
