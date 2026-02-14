033e7aafbb2aa4c0f25c4014aa9bf1914d846307
dmoke <dmytromykhaliukk@gmail.com>
2026-02-15 00:10:54 +0200

    ref delet

--- js/deleteIfOccurrenceMoreThan.js
+++ js/deleteIfOccurrenceMoreThan.js
@@ -1,20 +1,10 @@
 function deleteNth(arr, n) {
-  [...new Set(arr)].forEach((e) => {
-    let occurrences = arr
-      .sort((a, b) => a - b)
-      .reduce((acc, next) => (next === e ? acc + 1 : acc), 0);
-
-    if (n < occurrences)
-      arr = arr
-        .join(",")
-        .replace(
-          new Array(occurrences).fill(e).join(","),
-          new Array(n).fill(e).join(","),
-        )
-        .split(",")
-        .map(Number);
+  const seen = {};
+  return arr.filter((x) => {
+    seen[x] = (seen[x] || 0) + 1;
+    return seen[x] <= n;
   });
-  return arr;
 }
+
 
 console.log(deleteNth([20,37,20,21], 1));
