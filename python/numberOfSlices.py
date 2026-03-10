133083ff298b6fc610dd02f4f2f908eadf9ed91f
dmoke <dmytromykhaliukk@gmail.com>
2026-03-10 22:06:30 +0200

    add: slices kata

---
+++ python/numberOfSlices.py
@@ -0,0 +1,12 @@
+def series_slices(digits, n):
+   if n > len(digits):
+      raise ValueError("n must be less than or equal to the length of digits") 
+   result = [[*[int(d) for d in digits][i: i + n]] for i in range(len(digits))]
+   return [s for s in result if len(s) == n]
+
+
+
+
+
+
+print(series_slices("01234", 2))
